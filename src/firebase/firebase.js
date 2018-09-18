5990e3a6d0173d82ccc0428fb68442a6c556035b
Gerardo Grinman <ggrinman@gmail.com>
2018-09-18 08:19:29 -0300

    add firebase tests

---
+++ src/firebase/firebase.js
@@ -0,0 +1,152 @@
+import * as firebase from 'firebase';
+
+const config = {
+	apiKey: "AIzaSyAU-pa1blR-g53RraUarMH1kkLGLJbt9Yc",
+	authDomain: "expensify-b7fe4.firebaseapp.com",
+	databaseURL: "https://expensify-b7fe4.firebaseio.com",
+	projectId: "expensify-b7fe4",
+	storageBucket: "expensify-b7fe4.appspot.com",
+	messagingSenderId: "413484519294"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+// database.ref('expenses')
+// 	.once('value')
+// 	.then((snapshot) => {
+// 		const expenses = [];
+//
+// 		snapshot.forEach((childSnapshot) => {
+// 			expenses.push({
+// 				id: childSnapshot.key,
+// 				...childSnapshot.val()
+// 			});
+// 		});
+//
+// 		console.log(expenses);
+//
+// 	})
+// 	.catch((e) => {
+// 		console.log('Error fetching data', 2);
+// 	});
+
+database.ref().on('child_removed', (snapshot) => {
+
+	const expenses = [];
+
+	snapshot.forEach((childSnapshot) => {
+		expenses.push({
+			id: childSnapshot.key,
+			...childSnapshot.val()
+		});
+	});
+
+	console.log(expenses);
+
+});
+
+database.ref().on('child_changed', (snapshot) => {
+
+	const expenses = [];
+
+	snapshot.forEach((childSnapshot) => {
+		expenses.push({
+			id: childSnapshot.key,
+			...childSnapshot.val()
+		});
+	});
+
+	console.log(expenses);
+
+});
+
+database.ref().on('child_added', (snapshot) => {
+
+	const expenses = [];
+
+	snapshot.forEach((childSnapshot) => {
+		expenses.push({
+			id: childSnapshot.key,
+			...childSnapshot.val()
+		});
+	});
+
+	console.log(expenses);
+
+});
+
+// database.ref('expenses').push({
+// 	description: 'desc 1',
+// 	note: 'note 1',
+// 	amount: 101,
+// 	createdAt: 0
+// });
+//
+// database.ref('expenses').push({
+// 	description: 'desc 2',
+// 	note: 'note 2',
+// 	amount: 100,
+// 	createdAt: 0
+// });
+//
+// database.ref('expenses').push({
+// 	description: 'desc 3',
+// 	note: 'note 3',
+// 	amount: 100,
+// 	createdAt: 0
+// });
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+// 	console.log(snapshot.val());
+// });
+//
+// setTimeout(() => {
+// 	database.ref('age').set(30	);
+// }, 3500);
+//
+// setTimeout(() => {
+// 	database.ref().off('value', onValueChange);
+// }, 7000);
+
+// database.ref()
+// 	.once('value')
+// 	.then((snapshot) => {
+// 		const val = snapshot.val();
+// 		console.log(val);
+// 	})
+// 	.catch((e) => {
+// 		console.log('Error fetching data', 2);
+// 	});
+
+// database.ref().set({
+// 	name: 'Gerardo Grinman',
+// 	age: 33,
+// 	stressLevel: 6,
+// 	job: {
+// 		title: 'Software developer',
+// 		company: 'Google'
+// 	},
+// 	location: {
+// 		city: 'Philadelphia',
+// 		country: 'United States'
+// 	}
+// }).then(() => {
+// 	console.log('Data is saved');
+// }).catch((error) => {
+// 	console.log('This failed', error);
+// });
+//
+// database.ref().update({
+// 	name: 'Mike',
+// 	age: 29,
+// });
+
+// database.ref()
+// 	.remove()
+// 	.then(() => {
+// 	console.log('Data was removed');
+// }).catch((error) => {
+// 	console.log('Did not remove data', error);
+// });
--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
-import getVisibleExpenses from "./selectors/expenses";
+// import getVisibleExpenses from "./selectors/expenses";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
+import './firebase/firebase';
 
 const store = configureStore();
 
-// store.dispatch(addExpense({description: 'Water bill', amount: 100, createdAt: 0}));
-// store.dispatch(addExpense({description: 'Gas bill', amount: 200, createdAt: 1000}));
-// store.dispatch(addExpense({description: 'Rent', amount: 109500, createdAt: 0}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
 	<Provider store={store}>
