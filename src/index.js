f881ebfe8a49955e73634cb43c82cc7a46d58c09
Gerardo Grinman <ggrinman@gmail.com>
2018-09-21 08:56:43 -0300

    add login logout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, {history} from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {startSetExpenses} from "./actions/expenses";
 import registerServiceWorker from './registerServiceWorker';
@@ -9,13 +9,9 @@ import registerServiceWorker from './registerServiceWorker';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import {firebase} from './firebase/firebase';
 
 const store = configureStore();
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
 
 const jsx = (
 	<Provider store={store}>
@@ -23,12 +19,29 @@ const jsx = (
 	</Provider>
 );
 
+let hasRendered = false;
+
+const renderApp = () => {
+	if (!hasRendered) {
+		ReactDOM.render(jsx, document.getElementById('root'));
+		hasRendered = true;
+	}
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
 
-store.dispatch(startSetExpenses()).then(() => {
-
-	ReactDOM.render(jsx, document.getElementById('root'));
-
+firebase.auth().onAuthStateChanged((user) => {
+	if (user) {
+		store.dispatch(startSetExpenses()).then(() => {
+			renderApp();
+			if (history.location.pathname === '/') {
+				history.push('/dashboard');
+			}
+		});
+	} else {
+		renderApp();
+		history.push('/');
+	}
 });
 
 registerServiceWorker();
