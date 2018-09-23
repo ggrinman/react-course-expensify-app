faf3cb801baa9ff4973d6605389f016bd01f464a
Gerardo Grinman <ggrinman@gmail.com>
2018-09-23 14:52:31 -0300

    add styles

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import {firebase} from './firebase/firebase';
+import LoadingPage from './components/LoadingPage'
 
 const store = configureStore();
 
@@ -29,7 +30,7 @@ const renderApp = () => {
 	}
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('root'));
+ReactDOM.render(<LoadingPage />, document.getElementById('root'));
 
 firebase.auth().onAuthStateChanged((user) => {
 	if (user) {
